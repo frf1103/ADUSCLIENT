6a73d67bbbfb966c847d70f93a3492fdc3c914d4
francisco.filho <francisco.filho@siagri.com.br>
2025-04-21 23:00:39 -0300

    Mudancas 21-04

--- ParametrosGuruViewModel.cs
+++ ParametrosGuruViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,32 @@ namespace ADUSClient
     public class ParametrosGuruViewModel
     {
         public int id { get; set; }
+
+        [DisplayName("Token")]
         public string token { get; set; }
+
+        [DisplayName("Última Data")]
         public DateTime ultdata { get; set; }
+
+        [DisplayName("URL Subscriptions")]
         public string urlsub { get; set; }
+
+        [DisplayName("URL Transações")]
         public string urltransac { get; set; }
+
+        [DisplayName("Transação Padrão")]
+        public int? idtransacao { get; set; }
+
+        [DisplayName("Categoria Padrão")]
+        public int? idcategoria { get; set; }
+
+        [DisplayName("Conta Padrão")]
+        public string? idconta { get; set; }
+
+        [DisplayName("Parceiro Padrão")]
+        public string? idparceiro { get; set; }
+
+        [DisplayName("Centro de Custo Padrão")]
+        public int? idccusto { get; set; }
     }
 }
