35c0435eefe4ec5662859cac288e0baa7189d98f
francisco.filho <francisco.filho@siagri.com.br>
2025-07-16 23:25:14 -0300

    Commit 16.07 final

--- Parcela/visaogeralviewmodel.cs
+++ Parcela/visaogeralviewmodel.cs
@@ -23,6 +23,8 @@ namespace ADUSClient.Parcela
         public decimal valortotal { get; set; }
         public int arvores { get; set; }
         public decimal acompensar { get; set; }
+        public string fone { get; set; }
+        public string email { get; set; }
     }
 }
 
